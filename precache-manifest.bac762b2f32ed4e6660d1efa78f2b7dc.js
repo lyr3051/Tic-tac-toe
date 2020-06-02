b6df81516cc35f7423ab80ecfbadc6590db62929
LIN YI JOU <43023893+lyr3051@users.noreply.github.com>
2020-06-02 20:46:10 +0800

    Updates

---
+++ precache-manifest.bac762b2f32ed4e6660d1efa78f2b7dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ca9b6a63210489b606a43eb7d351f9",
+    "url": "/Tic-tac-toe/index.html"
+  },
+  {
+    "revision": "9d859a7048a0c58a0f6d",
+    "url": "/Tic-tac-toe/static/css/main.bede27b6.chunk.css"
+  },
+  {
+    "revision": "5a6d28e56b96db7a9d27",
+    "url": "/Tic-tac-toe/static/js/2.ad380614.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-tac-toe/static/js/2.ad380614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d859a7048a0c58a0f6d",
+    "url": "/Tic-tac-toe/static/js/main.52535c84.chunk.js"
+  },
+  {
+    "revision": "b96ffab54e861c03c2ff",
+    "url": "/Tic-tac-toe/static/js/runtime-main.8d917dda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
